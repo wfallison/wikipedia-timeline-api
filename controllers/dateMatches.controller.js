1b69f43557de5d84c951a6cfb474cde813568cd7
Billy Allison <williamfallison@gmail.com>
2022-08-19 20:42:47 -0700

    Adds until to prefixes for regex string

--- controllers/dateMatches.controller.js
+++ controllers/dateMatches.controller.js
@@ -11,7 +11,7 @@
 
 export const getDateMatches = (sentence) => {
 
-    const regex = /\b(on |.On |in |.In |as of |the |.The |of |around |a)\b(\b\d{1,2}\D{0,3})?\b(?:Jan(?:uary)?|Feb(?:ruary)?|Mar(?:ch)?|Apr(?:il)?|May|Jun(?:e)?|Jul(?:y)?|Aug(?:ust)?|Sep(?:tember)?|Oct(?:ober)?|(Nov|Dec)(?:ember)?)\D?(\d{1,2}\D?)?\D?((15[1-9]\d|16[1-9]\d|17[1-9]\d|18[1-9]\d|19[1-9]\d|20\d{2})|\d{2})|(The (15\d{2}|16\d{2}|17\d{2}|18\d{2}|19\d{2}|20\d{2}))|(In (15\d{2}|16\d{2}|17\d{2}|18\d{2}|19\d{2}|20\d{2}))|(Of (15\d{2}|16\d{2}|17\d{2}|18\d{2}|19\d{2}|20\d{2}))|(Around (15\d{2}|16\d{2}|17\d{2}|18\d{2}|19\d{2}|20\d{2}))/gi;
+    const regex = /\b(on |.On |in |.In |as of |the |.The |of |around |until |a)\b(\b\d{1,2}\D{0,3})?\b(?:Jan(?:uary)?|Feb(?:ruary)?|Mar(?:ch)?|Apr(?:il)?|May|Jun(?:e)?|Jul(?:y)?|Aug(?:ust)?|Sep(?:tember)?|Oct(?:ober)?|(Nov|Dec)(?:ember)?)\D?(\d{1,2}\D?)?\D?((15[1-9]\d|16[1-9]\d|17[1-9]\d|18[1-9]\d|19[1-9]\d|20\d{2})|\d{2})|(The (15\d{2}|16\d{2}|17\d{2}|18\d{2}|19\d{2}|20\d{2}))|(In (15\d{2}|16\d{2}|17\d{2}|18\d{2}|19\d{2}|20\d{2}))|(Of (15\d{2}|16\d{2}|17\d{2}|18\d{2}|19\d{2}|20\d{2}))|(Around (15\d{2}|16\d{2}|17\d{2}|18\d{2}|19\d{2}|20\d{2}))/gi;
     const bcDatesRegex = /\d{1,} BC|\d{1,}BC|\d{1,}AD |\d{1,} AD /gi
 
     /* ============== NOT HANDLED ==============
@@ -46,6 +46,9 @@ export const getDateMatches = (sentence) => {
           time ago in MA (Millions), time ago in GA (Billions))
           Read more:
           https://en.wikipedia.org/wiki/Year#SI_prefix_multipliers
+        > millennia later
+        > millennia before
+        > Until
 
           > Infer October 12, 1814
             Instead of 
