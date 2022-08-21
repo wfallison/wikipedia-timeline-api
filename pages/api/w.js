c12974c9753348de1768451fea10992d27bd6686
Billy Allison <williamfallison@gmail.com>
2022-08-20 20:53:35 -0700

    Attempt to setup cache

--- pages/api/w.js
+++ pages/api/w.js
@@ -20,7 +20,7 @@ const cors = initMiddleware(
 
 export default async function handler(req, res) {
   await cors(req, res)
-  res.header('Access-Control-Max-Age', 60000 * 60 * 24);
+  res.setHeader('Access-Control-Max-Age', 60000 * 60 * 24);
   var obj = req.body;
   const data = await handleMultipleResults(obj)
   res.send(data) 
