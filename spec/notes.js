08a607753dd0710fc065b146fadfafaa037b9618
Billy Allison <williamfallison@gmail.com>
2022-08-22 18:56:19 -0700

    Tag version 0.0.0

---
+++ spec/notes.js
@@ -0,0 +1,65 @@
+
+ /* ============== NOT HANDLED ==============
+ > around the year 270 BC.
+ > in the 7th century BC,
+ > dating back to the 3rd century AD
+ > the year 270 BC
+ > 600–1000 CE
+ > late 1590s
+ > from the late 1590s
+ > From 1817 to 1823
+ > since at least the early 1660s
+ > after 1600 // not to be confused with a 
+              // number like, 'after 1600 
+              // bottles of...'
+ > in 2007–2008  ?
+ > in the 1560s–70s
+ > between 1540 and 1818
+ > c. 1471–1493
+ > 1810–1814
+ > After several efforts, Spanish troops 
+   from Peru took advantage of the 
+   internecine strife to reconquer Chile 
+   in 1814, when they reasserted control 
+   by the Battle of Rancagua on 
+   October 12. O'Higgins, Carrera and 
+   many of the Chilean rebels escaped 
+   to Argentina.
+ > kyr, myr, byr, kya, mya, bya
+   (Thousand, Million, Billion, Time ago in Ka (Thousands), 
+   time ago in MA (Millions), time ago in GA (Billions))
+   Read more:
+   https://en.wikipedia.org/wiki/Year#SI_prefix_multipliers
+ > millennia later
+ > millennia before
+   > Infer October 12, 1814
+     Instead of 
+         - October 12, 1970
+         - January 1, 1814
+ > "differing by a factor of 20000"
+     results in: of 2000 (1/1/2000)
+
+     ideas:
+     The {noun} of {year} - good
+     a {noun} of {number} {noun} - not good
+     a group of 2000 people
+
+==========================================
+================ BUGS ====================
+ > to a depth of 2000 m. (Earth Article)    
+     ==> The year 2000
+ > In Mark 14 "In Mark 14:61 the high..."   
+     ==> A not real date "Mark 14"
+ > 1683.3 /sqmi.
+     ==> from Las Vegas article
+ > in Oregon with more than 50,000 adherents
+     ==> 000 AD
+     *Poorly fixed*
+ > Eastern Oregon is sparsely populated but is 
+   home to Hermiston which with a population of
+   18000 is the largest and fastest-growing 
+   city in the region.
+     ==> (year) of 1800
+     ==> Should not appear at all, as its a 
+         description of the population size
+*/ 
