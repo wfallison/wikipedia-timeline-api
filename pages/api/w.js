a89134f0bafcdbd74ec8ad85472ad5f2efa0e6d1
Billy Allison <williamfallison@gmail.com>
2023-02-23 21:01:05 -0800

    Lookup articles in async fashion and await all

--- pages/api/w.js
+++ pages/api/w.js
@@ -9,22 +9,52 @@ export default async function handler(req, res) {
 
 async function fetchArticles(obj){
 
-  let allArticles = [];
-  let allHeaders = [];
+  // let allArticles = [];
+  // let allHeaders = [];
 
-  for (const el of obj) {
-    const item = await fetchWtf(el.articleTitle)
-      if (item){
-        allArticles = allArticles.concat(item.sorted)
-        allHeaders = allHeaders.concat(item.headerData)
-      }
-  }
+  // for (const el of obj) {
+  //   const item = await fetchWtf(el.articleTitle)
+  //     if (item){
+  //       allArticles = allArticles.concat(item.sorted)
+  //       allHeaders = allHeaders.concat(item.headerData)
+  //     }
+  // }
   
-  const sorted = allArticles.sort((a, b) => {
-    return a.dateSortMs - b.dateSortMs;
+  const promises = obj.map(async (el) => {
+    const item = await fetchWtf(el.articleTitle);
+    if (item) {
+      return {
+        sorted: item.sorted,
+        headerData: item.headerData
+      };
+    }
   });
 
-  return {sorted: sorted,
-          headers: allHeaders
-        };
+  const results = await Promise.all(promises);
+
+  // const sorted = allArticles.sort((a, b) => {
+  //   return a.dateSortMs - b.dateSortMs;
+  // });
+
+  const allArticles = results.reduce((acc, curr) => {
+    if (curr) {
+      return acc.concat(curr.sorted);
+    }
+    return acc;
+  }, []);
+
+  const allHeaders = results.reduce((acc, curr) => {
+    if (curr) {
+      return acc.concat(curr.headerData);
+    }
+    return acc;
+  }, []);
+
+  // return {
+  //   allArticles,
+  //   allHeaders
+  // };
+  return {sorted: allArticles,
+    headers: allHeaders
+  };
 }
